f175fee669db65f8571275db4d7a0d51e83fd0f2
Dea Dressel <ddressel@stanford.edu>
2023-05-24 13:12:44 -0700

    money data and contour moves

--- find_squirrels.jl
+++ find_squirrels.jl
@@ -1,4 +1,4 @@
-using GaussianProcesses, CSV, DataFrames, GeoStats
+using GaussianProcesses, Plots, CSV, DataFrames, Geodesy #, PlotlyJS
 
 
 data = CSV.read("nyc_squirrels.csv", DataFrame)
@@ -8,34 +8,53 @@ lat_max = maximum(data[:,"lat"])
 long_min = minimum(data[:,"long"])
 long_max = maximum(data[:,"long"])
 
-lower_left = LatLon(lat_min, long_min)
-lower_right = LatLon(lat_min, long_max)
-upper_left = LatLon(lat_max, long_min)
+origin_LLA = LLA(lat_min, long_min, 0.0)
+lower_right_LLA = LLA(lat_min, long_max, 0.0)
+upper_left_LLA = LLA(lat_max, long_min, 0.0)
 
-x_range = haversine(lower_left, lower_right)
-y_range = haversine(lower_left, upper_left)
+origin_ENU = ENU(origin_LLA, origin_LLA, wgs84)
+lower_right_ENU = ENU(lower_right_LLA, origin_LLA, wgs84)
+upper_left_ENU = ENU(upper_left_LLA, origin_LLA, wgs84)
 
-println("lower_left: ", lower_left)
-println("lower_right: ", lower_right)
-println("upper_left: ", upper_left)
+println("origin_ENU: ", origin_ENU)
+println("lower_right_ENU: ", lower_right_ENU)
+println("upper_left_ENU: ", upper_left_ENU)
+
+x_range = ceil(lower_right_ENU[1])
+y_range = ceil(upper_left_ENU[2])
+
 println("x_range: ", x_range)
 println("y_range: ", y_range)
 
+bin_size = 100
+
+x_axis = range(0.0, stop=x_range, step=bin_size)
+y_axis = range(0.0, stop=y_range, step=bin_size)
+
+x_bins = collect(x_axis)
+y_bins = collect(y_axis)
+
+grid = zeros(length(x_bins), length(y_bins))
+println("grid.size: ", size(grid))
 
 
+for row in eachrow(data)
+    lla = LLA(row["lat"], row["long"], 0.0)
+    enu = ENU(lla, origin_LLA, wgs84)
+    x,y = enu[1]/bin_size, enu[2]/bin_size
 
-# println("min lat: ", minimum(data[:,"lat"]))
-# println("max lat: ", maximum(data[:,"lat"]))
-# println("lat diff: ", )
-# println("min long: ", minimum(data[:,"long"]))
-# println("max long: ", maximum(data[:,"long"]))
+    x = max(x,0)
+    y = max(y,0)
+    
+    grid[floor(Int64, x)+1, floor(Int64, y)+1] += 1
+end
 
-# # observed x and y values
-# x_obs = []
-# y_obs = []
+println(sum(grid))
 
-# for row in data
-#     push!( (row["lat"],row["long"]), x_obs )
+println("length(x_axis): ", length(x_axis))
+println("length(y_axis): ", length(y_axis))
+# println(grid[70:100, 70:100])
 
-
-# end
+plt = contourf(x_axis, y_axis, grid', levels =20, c = cgrad(:viridis, rev = true), legend = true, title = "contour", xlabel = "x_bins", ylabel = "y_bins")
+display(plt)
+savefig("contour_100")
